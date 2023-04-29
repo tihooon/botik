f2ffc3a3c5a888f36f899d56ea6407bfe80717be
cumushek <valeushim@duck.com>
2023-04-29 14:05:29 +0300

    Исправление бага

--- Texts/txtToMsg.py
+++ Texts/txtToMsg.py
@@ -1,2 +1,5 @@
-with open("Abuz1.txt", 'r') as txt:
+import os
+ROOT = lambda base : os.path.join(os.path.dirname(__file__), base).replace('\\','/')
+
+with open(ROOT('Abuz1.txt'), 'r', encoding="utf-8") as txt:
     Abuz1 = txt.read()
